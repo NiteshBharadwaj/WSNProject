e226efea0b766f007fda610c9c2813bf8eea3c68
NiteshBharadwaj <ee2110514@ee.iitd.ernet.in>
2014-10-14 19:34:14 +0530

    Create receive_samples.py
    To receive packets from a module connected via USB and print them to python terminal.

---
+++ PythonInterface/receive_samples.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+from xbee.zigbee import ZigBee
+import serial
+
+PORT = 'COM4'
+BAUD_RATE = 9600
+
+# Open serial port
+ser = serial.Serial(PORT, BAUD_RATE)
+
+# Create API object
+xbee = ZigBee(ser)
+
+# Continuously read and print packets
+while True:
+    try:
+        response = xbee.wait_read_frame()
+        print response
+    except KeyboardInterrupt:
+        break
+        
+ser.close()
